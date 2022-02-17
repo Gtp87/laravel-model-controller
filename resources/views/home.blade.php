be88f6f884dbf2404e8840fe0f71c073f4aa85bf
Gtp87 <fabio.giannitrapani@gmail.com>
2022-02-17 15:47:03 +0100

    fix nationality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="card w-25 m-3 p-3 bg-light">
             <h1>Title: {{$movie->title}}</h1>
             <h3>Original Title: {{$movie->original_title}}</h3>
-            <h5>Nation: {{$movie->nationality}}</h5>
+            <h5>Nationality: {{$movie->nationality}}</h5>
             <h5>Date: {{$movie->date}}</h5>
             <h5>Vote: {{$movie->vote}}</h5>
         </div>   
